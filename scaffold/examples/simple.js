17cc3e26793060631b6ce2df3f1ccd62b458ba19
yanweiqing <yanweiqing@youzan.com>
2016-05-15 12:44:32 +0800

    添加eslint 优化 init 命令逻辑

--- scaffold/examples/simple.js
+++ scaffold/examples/simple.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Seed from '../src/Index.js';
 import '../assets/index.scss';
 
+/*
+## Write Something here
+
+You can write guides for users here
+*/
 const Simple = function(props) {
     return <Seed />
 }
