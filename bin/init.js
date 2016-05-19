be696527c7d00ae1d995d58fad4df06b39be1a3d
yanweiqing <yanweiqing@youzan.com>
2016-05-19 11:26:07 +0800

    save

--- bin/init.js
+++ bin/init.js
@@ -13,16 +13,20 @@ var exec = ch.exec;
 module.exports = function(name) {
 
     // 项目规范文件拷贝
-    gulp.task('copy', function() {
+    gulp.task('copy', function(callback) {
         logger('----> 开始初始化')
 
-        return gulp.src(path.resolve(__dirname, '../scaffold/**/*'), { dot: true })
-            .pipe(gulp.dest(path.resolve(projectPath, './' + name)));
+        exec('git clone git@gitlab.qima-inc.com:zent/zent-seed.git ' + name, function(err, stdout, stderr) {
+            logger('-------> 拉取 zent-seed');
+            exec('rm -rf ./'+ name + '/.git', function(err, stdout, stderr) {
+                callback();
+            });
+        });
     });
 
     // 单独再导入readme
-    gulp.task('init:readme', function() {
-        logger('----> 生成readme');
+    gulp.task('init:readme', function(callback) {
+        logger('-------> 生成 readme');
 
         var documentation = fs.readFileSync(path.resolve(__dirname, '../readme.md'));
         var fileName = path.resolve(projectPath, './' + name + '/readme.md');
@@ -31,30 +35,36 @@ module.exports = function(name) {
             documentation: documentation
         });
         fs.writeFile(fileName, file);
+
+        callback();
     });
 
     // 单独再导入package.sjon
-    gulp.task('init:package', function() {
-        logger('----> 生成package.json');
+    gulp.task('init:package', function(callback) {
+        logger('-------> 生成 package.json');
 
         var fileName = path.resolve(projectPath, './' + name + '/package.json');
         var file = _.template(fs.readFileSync(fileName))({
             name: name
         });
         fs.writeFile(fileName, file);
+
+        callback();
     });
 
     // 项目依赖安装
-    gulp.task('install', function() {
-        logger('----> 安装依赖....')
+    gulp.task('install', function(callback) {
+        logger('-------> 安装 依赖....')
 
         var dependencies = ['react', 'eslint', 'eslint-config-airbnb', 'eslint-plugin-jsx-a11y', 'eslint-plugin-react'];
         exec('which npm', function(err, stdout, stderr) {
             var Npath = stdout.toString().trim();
             var command = Npath + ' i --registry="http://registry.npm.qima-inc.com" ' + dependencies.join(' ');
+            console.log(command);
             exec(command, {cwd: projectPath + '/' + name}, function(err, stdout, stderr) {
                 logger(stdout + '');
                 logger('----> 初始化完成！')
+                callback();
             });
         });
     });
