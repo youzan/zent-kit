f8fef354291d7fcb633a16e5840356ceae94e236
yanweiqing <yanweiqing@youzan.com>
2016-05-27 18:29:26 +0800

    版本提示，待优化

--- bin/information.js
+++ bin/information.js
@@ -2,10 +2,22 @@
 var path = require('path');
 var ch = require('child_process');
 var config = require('../package.json');
+var gutil = require('gulp-util');
 var logger = console.log.bind(console);
 
 var loggerVersion = function() {
     logger('Using %s@%s', config.name, config.version)
+}
+
+var logRepeat = function(func, version, time) {
+    while(version > 0) {
+        var t = time;
+        while(t > 0) {
+            func();
+            t --;
+        }
+        version --;
+    }
 }
 
 var exec = ch.exec;
@@ -28,7 +40,26 @@ module.exports = function(dir) {
         var Npath = stdout.toString().trim();
         var command = Npath + ' view --registry="http://registry.npm.qima-inc.com" @youzan/zent-kit version';
         exec(command, function(err, stdout, stderr) {
-            logger('Latest Version %s    on              http://npm.qima-inc.com/', stdout);
+            logger('Latest Version %s    on              http://npm.qima-inc.com/\n', stdout);
+
+            // 对版本进行判别，这边的逻辑将来抽象出来
+            var version = [];
+            version.push(config.version.split('.'));
+            version.push(stdout.split('.'));
+            var versionM = version[1][2].slice(0, -1) - version[0][2];
+            if (version[0][1] !== version[1][1] || version[0][0] !== version[1][0]) {
+                versionM = 100;
+            }
+
+            if (versionM >= 10) {
+                logRepeat(function() {
+                    gutil.log(gutil.colors.red('! ! !：') + gutil.colors.yellow('当前版本已经严重落后，请尽快升级'));
+                }, versionM, 10);
+            } else if (versionM >= 5) {
+                logRepeat(function() {
+                    gutil.log(gutil.colors.yellow('~ ~ ~：') + gutil.colors.green('当前版本已经严重落后，请尽快升级'));
+                }, versionM, 5);
+            }
         })
     });
 }
