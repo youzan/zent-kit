cf45167f8f4bcb6292c7b085e7bf88505d53b86e
zhangmengxuan <zhangmengxuan@youzan.com>
2016-07-26 10:24:03 +0800

    让css忽略后缀，把devtool重新开启

--- webpack/webpack.build.js
+++ webpack/webpack.build.js
@@ -1,23 +1,25 @@
 var LiveReloadPlugin = require('webpack-livereload-plugin');
 var precss = require('precss');
 var autoprefixer = require('autoprefixer');
-var postcssImport = require('postcss-import');
+var postcssImport = require('@youzan/postcss-import');
 
 module.exports = function(entry, output) {
     var webpackBase = require('./webpack.base.js')(entry, output);
     var webpackConfig = {
         watch: true,
-        // devtool: 'source-map', // 暂时关闭，新创建的组件会有编译的问题
+        devtool: 'eval-source-map', // 暂时关闭，新创建的组件会有编译的问题
         postcss: function (webpack) {
-			return [
-				postcssImport({
-					onImport: function (files) {
-				        files.forEach(this.addDependency)
-					}.bind(webpack),
-				}),
-				precss,
-				autoprefixer
-			];
+          return [
+            postcssImport({
+              addDependencyTo: webpack,
+              extensions: [
+                '.scss',
+                '.css'
+              ]
+            }),
+            precss,
+            autoprefixer
+    			];
         },
         plugins:[new LiveReloadPlugin()]
     };
