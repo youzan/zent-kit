6c139fceef8894a83de6abb9ab1d019ac97dc612
yanweiqing <yanweiqing@youzan.com>
2016-08-03 15:17:21 +0800

    seed 添加 felint 相关的文件

--- bin/init.js
+++ bin/init.js
@@ -54,7 +54,7 @@ module.exports = function(name) {
     });
 
     // 项目依赖安装
-    gulp.task('install', function(callback) {
+    gulp.task('install:npm', function(callback) {
         gutil.log('-------> 安装 依赖....');
 
         exec('which npm', function(err, stdout, stderr) {
@@ -62,16 +62,38 @@ module.exports = function(name) {
             var command = Npath + ' i --registry="http://registry.npm.qima-inc.com"';
             exec(command, {cwd: projectPath + '/' + name}, function(err, stdout, stderr) {
                 logger(stdout + '');
-                gutil.log(gutil.colors.green('----> 初始化完成！'));
                 callback();
             });
         });
     });
 
+    gulp.task('install:felint', function(callback) {
+        gutil.log('------> 初始化 felint');
+
+        exec('cd ' + name + '; felint init -6', function(err, stdout, stderr) {
+          if (err) {
+            gutil.log(gutil.colors.red(stderr));
+            gutil.log(guitl.colors.red('执行 felint 指令失败，请检查本地 felint 是否正确安装后重新运行zent-kit'));
+          }
+          callback();
+        });
+    })
+
+    // 初始化 git，本步骤非必须，未来可能添加一定功能
+    gulp.task('install:git', function(callback) {
+        gutil.log('------> 初始化 git');
+
+        exec('cd ' + name + '; pwd; git init', function(err, stdout, stderr) {
+          // do nothing now
+          gutil.log(gutil.colors.green('----> 初始化完成！'));
+          callback();
+        });
+    });
 
     if (!name) {
         logger('   sir: we need a project name');
         return;
     }
-    runSequence('copy', 'init:readme', 'init:package', 'install');
+
+    runSequence('copy', 'init:readme', 'init:package', 'install:npm', 'install:felint', 'install:git');
 };
