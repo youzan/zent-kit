2644d73172cd07144d9a7a547e247610065c4729
zhangmengxuan <zhangmengxuan@youzan.com>
2016-08-04 21:13:00 +0800

    删除多余的代码

--- webpack/postcss.conf.js
+++ webpack/postcss.conf.js
@@ -2,10 +2,6 @@ var precss = require('precss');
 var autoprefixer = require('autoprefixer');
 var postcssImport = require('@youzan/postcss-import');
 var inlineComment = require('postcss-inline-comment');
-var atVariables = require('postcss-at-rules-variables');
-var atIf = require('postcss-conditionals');
-var atEach = require('postcss-each');
-var atFor = require('postcss-for');
 
 module.exports = function(webpack, isDynamic) {
     var plugins = [];
@@ -13,10 +9,6 @@ module.exports = function(webpack, isDynamic) {
     if (isDynamic) {
         plugins.push(postcssImport({
             addDependencyTo: webpack,
-            plugins: [
-                require('postcss-at-rules-variables'),
-                require('@youzan/postcss-import')
-            ],
             extensions: [
                 '.scss',
                 '.css'
@@ -24,7 +16,6 @@ module.exports = function(webpack, isDynamic) {
         }));
     } else {
         plugins.push(postcssImport({
-            addDependencyTo: webpack,
             extensions: [
                 '.scss',
                 '.css'
@@ -34,14 +25,9 @@ module.exports = function(webpack, isDynamic) {
 
     plugins = plugins.concat([
         inlineComment,
-        atVariables,
-        atEach,
-        atFor,
-        atIf,
         precss,
         autoprefixer
     ]);
 
-    console.log(plugins);
     return plugins;
 };
